b08ed0d85c7b4eccff034d088dff672dbf3550f7
Joshua Hockerman <jhockerman96@gmail.com>
2021-12-11 01:27:04 -0600

    Made it so that each item in the menu linked to its own page that contained the toppings for the pizza. Adding images next.

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -9,4 +9,5 @@ app_name = "pizzas"
 urlpatterns = [
     path("", views.index, name="index"),
     path("menu", views.menu, name="menu"),
+    path("menu/<int:pizza_id>/", views.toppings, name="toppings"),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -14,3 +14,12 @@ def menu(request):
     items = Pizza.objects.order_by("name")
     context = {"items": items}
     return render(request, "pizzas/menu.html", context)
+
+
+def toppings(request, pizza_id):
+    """Show a single pizza and all of its toppings."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by("name")
+    context = {"pizza": pizza, "toppings": toppings}
+
+    return render(request, "pizzas/toppings.html", context)
